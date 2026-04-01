7360a45905428b4fc6b000027fee4d53fad344d5
MohanaPrasath51 <mohanaprasath1205@gmail.com>
2026-04-01 23:11:17 +0530

    Delete backend directory

--- backend/utils/asyncHandler.js
+++
@@ -1,7 +0,0 @@
-// utils/asyncHandler.js
-// Productivity Tool: Wraps async controllers to remove try/catch boilerplate
-const asyncHandler = (fn) => (req, res, next) => {
-  Promise.resolve(fn(req, res, next)).catch(next);
-};
-
-module.exports = asyncHandler;
